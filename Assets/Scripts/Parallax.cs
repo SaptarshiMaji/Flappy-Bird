83e9500fc8194aacb63a3161cbdfaec17b3cfb5b
USER <USER@SAPTARSHI>
2025-08-04 22:12:26 +0530

    Add project files.

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    public float aimationSpeed = 1f;
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(aimationSpeed * Time.deltaTime, 0); 
+    }
+}
